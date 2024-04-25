3824081eacc507606fe8349d37ae4fe633d6264f
Cintia Leal <82849825+cintialr@users.noreply.github.com>
2024-04-25 14:26:48 -0300

    Update ex4.c

--- Exercicios/Modulo 2/ex4.c
+++ Exercicios/Modulo 2/ex4.c
@@ -3,12 +3,12 @@
 #include <stdint.h>
 
 void main(){
-  
+
     // Configuração
-	WDTCTL = WDTPW | WDTHOLD;
-	
+    WDTCTL = WDTPW | WDTHOLD;
+
 // configuração dos pinos
-  
+
     P1OUT &= ~BIT0;                         // Configura o LED vermelho
     P1DIR |=  BIT0;                         // Saída por padrão em 0
     P4OUT &= ~BIT7;                         // Configura o LED verde
@@ -21,12 +21,17 @@ void main(){
     P2REN |=  BIT1;                         // Entrada com resistor
     P2OUT |=  BIT1;                         // de pull-up
 
-	// Configuração do timer
-	TA0CTL  = TASSEL__ACLK |                // Escolhe o ACLK como ref de tempo
-	          MC__UP       |                // Modo UP: Conta de 0 até CCR0
-	          TACLR;                        // Começa em 0
+    // Configuração do timer
+    TA0CTL  = TASSEL__ACLK |                // Escolhe o ACLK como ref de tempo
+              MC__UP       |                // Modo UP: Conta de 0 até CCR0
+              TACLR;                        // Começa em 0
 
-	TA0CCR0 = 32768/2 - 1;                  // Conta 0,5 segundo
+    TA0CCR0 = 32768/2 - 1;                  // Conta 0,5 segundo
 
-
+    while(1){       //loop infinito
+        if ( ((P1IN&BIT1) == 0) || ((P2IN&BIT1) == 0) )
+            P1OUT |= BIT0;
+        else
+            P1OUT &= ~BIT0;
+    }
 }
