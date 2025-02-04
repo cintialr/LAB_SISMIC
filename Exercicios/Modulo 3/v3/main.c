f5b898a2bd3fa2e8caf7c9ad073c6c13257d69bc
Cintia Leal <82849825+cintialr@users.noreply.github.com>
2025-02-04 19:11:08 +0000

    Visto 3

---
+++ Exercicios/Modulo 3/v3/main.c
@@ -0,0 +1,18 @@
+#include <msp430.h>
+#include <stdint.h>
+#include "uart.h"
+#include "adc.h"
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	volatile uint16_t adcResult;
+	TA0CCTL0 = MC__UP | TASSEL__SMCLK;
+	TA0CCR0  = 100 - 1;
+	TA0CCTL0 = CCIE;
+
+}
---
+++ Exercicios/Modulo 3/v3/uart.h
@@ -0,0 +1,23 @@
+#ifndef __UART_H
+#define __UART_H
+#include <msp430.h>
+#include <stdint.h>
+
+uint16_t count;
+uint8_t rxBuff[12], i;
+
+
+#define MAX_TIMERS  8
+#define TX          6,0
+#define RX          6,1
+#define TB          833,us
+
+
+typedef enum { us ,ms = 0, sec = 1, min = 60} timeunit_t;
+
+void uartOpen(uint8_t interface);
+void uartPrint(char * str);
+void wait(uint16_t time, uint16_t uint);
+void uartBang(uint8_t byte);
+
+#endif
