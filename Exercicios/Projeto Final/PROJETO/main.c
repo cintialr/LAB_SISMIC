310dffda8e260974bbc1a98245a550a6bcc8a57b
Cintia Leal <82849825+cintialr@users.noreply.github.com>
2025-02-17 14:06:58 -0300

    Update main.c

--- Exercicios/Projeto Final/PROJETO/main.c
+++ Exercicios/Projeto Final/PROJETO/main.c
@@ -5,68 +5,70 @@
 #include "lcd.h"
 #include "uart.h"
 
-// Definições do Modulo RTC DS1302
+// DefiniÃ§Ãµes do Modulo RTC DS1302
 #define RST  BIT2     // P3.2     RESET
 #define CLK  BIT4     // P3.4     Serial Clock
 #define DAT  BIT3     // P3.3     Data Input/Output
 
-#define BISSEXTO (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
-
-// Definições de alarme e hora
+// DefiniÃ§Ãµes de alarme e hora
 unsigned char hour, minute, second;
 unsigned char alarm_hour = 0, alarm_min = 0, alarm_sec = 0;
 unsigned char alarm_active = 0;
 
-// Definições de data
-unsigned int dayOfWeek, day, month, year; // Exemplo de 15/02/25
+// DefiniÃ§Ãµes de data
+unsigned char dayOfWeek, day, month, year; // Exemplo Sab, 15/02/25
 
 volatile uint8_t index= 0;
 volatile char rxBuffer[12];
 volatile uint8_t lcdBlinkState = 0;
 
 void RTC_SetTime(unsigned char h, unsigned char m, unsigned char s);
-void RTC_SetDay(unsigned char dayOfWeek, unsigned char d, unsigned char month, unsigned char y);
+void RTC_SetDay(unsigned char Day, unsigned char d, unsigned char m, unsigned char y);
 void RTC_ReadTime();
 void displayDateTime();
 void checkAlarm();
 void setupTimer();
 void processUART();
 void blinkLCD();
-char* getDayOfWeek(unsigned char day);
+char *getDayOfWeek(unsigned char day);
 
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD; // Parar o watchdog timer
-    // Configuração do timer
+    // ConfiguraÃ§Ã£o do timer
     TA0CTL  = TASSEL__ACLK |                // Escolhe o ACLK como ref de tempo
-              MC__UP       |                // Modo UP: Conta de 0 até CCR0
-              TACLR;                        // Começa em 0
+              MC__UP       |                // Modo UP: Conta de 0 atÃ© CCR0
+              TACLR;                        // ComeÃ§a em 0
 
     TA0CCR0  = 32768/2 - 1;                 // Conta 0,5 segundo
     TA0CCTL0 = CCIE;                        // CCIE - Habilitador local da INTpa
 
-    // configuração RTC
+    // configuraÃ§Ã£o RTC
 
     P3DIR |= RST + CLK + DAT;
     P3OUT &= ~(RST + CLK + DAT);
 
-    __enable_interrupt(); // Habilita interrupções globais
+    __enable_interrupt(); // Habilita interrupÃ§Ãµes globais
     i2cConfig();
     lcdInit();
     uartOpen(1); // Inicializa a UART
     // Dia da semana {0:"Dom"; 1:"Seg"; 2:"Ter"; 3:"Qua"; 4:"Qui"; 5:"Sex"; 6:"Sab"}
-    RTC_SetDay(2, 17, 02, 25);  // 6 = Sábado, 15 = Dia, FEV = 2 (constante), 25 = Ano
-    RTC_SetTime(10, 15, 1);
+    RTC_SetDay(2, 31, 12, 25);  //Dia da semana, Dia, mes, Ano
+    RTC_SetTime(23, 59, 0);
 
     uartPrint("Digite hh:mm:ss para configurar o alarme.\n");
 
     while (1) {
         processUART();
         displayDateTime();
+        __delay_cycles(1000000); // 1seg
         if (alarm_active) {
             checkAlarm();
             blinkLCD();
         }
-        __low_power_mode_3(); // Aguarda interrupções para otimizar energia
+        // estava desligando o LCD
+        __low_power_mode_1(); //Economiza energia sem desligar o LCD.
+        __bic_SR_register_on_exit(LPM1_bits);  // Sai do LPM1 apÃ³s interrupÃ§Ã£o
+
     }
 }
 
@@ -75,25 +77,54 @@ void RTC_SetTime(unsigned char h, unsigned char m, unsigned char s) {
     minute = m;
     second = s;
 }
-void RTC_SetDay(unsigned char day, unsigned char d, unsigned char month, unsigned char y){
-    dayOfWeek = day;
+void RTC_SetDay(unsigned char Day, unsigned char d, unsigned char m, unsigned char y){
+    dayOfWeek = Day;
     day = d;
-    month = month;
+    month = m;
     year = y;
 }
 
 void RTC_ReadTime() {
+        static const unsigned char daysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        second = (second + 1) % 60;
+        if (second == 0) {
+            minute = (minute + 1) % 60;
+            if (minute == 0) {
+                hour = (hour + 1) % 24;
+                if (hour == 0) { // Se chegou Ã  meia-noite, mudar o dia
+                    day++;
+
+                    // Verificar se o ano Ã© bissexto para ajustar fevereiro
+                    unsigned char maxDays = daysInMonth[month - 1]; // O array comeÃ§a do Ã­ndice 0 (janeiro)
+                    if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+                        maxDays = 29; // Ano bissexto
+                    }
+
+                    if (day > maxDays) { // AvanÃ§ar o mÃªs
+                        day = 1;
+                        month++;
+                        if (month > 12) { // AvanÃ§ar o ano
+                            month = 1;
+                            year++;
+                        }
+                    }
+
+                    dayOfWeek = (dayOfWeek + 1) % 7; // AvanÃ§a o dia da semana
+                }
+            }
+        }
 }
 
 void displayDateTime() {
-    char buffer[20];
-    char * printDAy = getDayOfWeek(dayOfWeek);
-    // Ex: Sab, 15/02/25 18:21:54 com quebra de linha
-    sprintf(buffer, "%s, %02u/%02u/%02u\n  %02u:%02u:%02u",
-            printDAy, day, month, year, hour, minute, second);
-    lcdPrint(buffer);  // Mostra no LCD
-    uartPrint(buffer);  // Exibe no console via UART
-    uartPrint("\n");
+    char linha1[20];
+    char linha2[20];
+
+    sprintf(linha1, "%s, %02u/%02u/%02u\n", getDayOfWeek(dayOfWeek), day, month, year);
+    sprintf(linha2, "  %02u:%02u:%02u\n", hour, minute, second);
+
+    lcdPrint((uint8_t *)linha1);
+    lcdPrint((uint8_t *)linha2);
 }
 
 void checkAlarm() {
@@ -149,58 +180,7 @@ char* getDayOfWeek(unsigned char day) {
 
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer_A_ISR(void) {
-    static const unsigned char daysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-/**
- * second = (second + 1) % 60
- * minute = (minute + 1) % 60
- * hour = (hour + 1) % 24;
- */
-
-    second++;
-    if (second == 60)
-        second = 0;
-
-    if (second == 0) {
-
-        minute++;
-        if (minute == 60)
-            minute = 0;
-
-        if (minute == 0) {
-            hour++;
-            if (hour == 24)
-                hour = 0;
-
-            if (hour == 0) { // Se chegou à meia-noite, mudar o dia
-                day++;
-
-                // Verificar se o ano é bissexto para ajustar fevereiro
-                unsigned char maxDays = daysInMonth[month - 1]; // O array começa do índice 0 (janeiro)
-            /*
-             *  if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
-                                maxDays = 29; // Ano bissexto
-                            }
-             */
-                if (day > maxDays) { // Avançar o mês
-                    day = 1;
-                    month++;
-                    if (month > 12) { // Avançar o ano
-                        month = 1;
-                        year++;
-                    }
-                }
-
-                //dayOfWeek = (dayOfWeek + 1) % 7; // Avança o dia da semana
-                dayOfWeek += 1;
-                if (dayOfWeek >= 7) {
-                    dayOfWeek = 0;
-                }
-            }
-        }
-    }
-
-
-
+    RTC_ReadTime();
     displayDateTime();     // Atualiza o display com a hora e data
     checkAlarm();          // Verifica se o alarme deve tocar
 }
